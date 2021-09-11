0dfc74372b2b3e80587541f2c6a0b888613debe9
Helton57 <a.andreenkov@rightech.io>
2021-09-12 00:09:47 +0300

    add UserJsonAdapter.kt with PrefObjDelegate.kt

--- app/src/main/java/ru/skillbranch/skillarticles/data/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/PrefManager.kt
@@ -16,7 +16,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.zip
 import ru.skillbranch.skillarticles.App
+import ru.skillbranch.skillarticles.data.adapters.UserJsonAdapter
 import ru.skillbranch.skillarticles.data.delegates.PrefDelegate
+import ru.skillbranch.skillarticles.data.delegates.PrefObjDelegate
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -39,6 +41,7 @@ class PrefManager(context: Context = App.applicationContext()) {
     var testString by PrefDelegate("test")
     var testBoolean by PrefDelegate(false)
 
+    var testUser by PrefObjDelegate(UserJsonAdapter())
 
     val settings: LiveData<AppSettings>
         get() {
